5d46e47fae3d2384e9c42ba40be513eda61f3be7
toburau <toburau@gmail.com>
2025-11-30 01:18:40 +0900

    add clear sequence

--- scripts/scene.gd
+++ scripts/scene.gd
@@ -6,6 +6,7 @@ extends Node3D
 @export var spawn_debris_count := 10
 
 var spawned_objects: Array = []  # ここに生成したノードを記録しておく
+var clear_flag = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -44,14 +45,22 @@ func clear_objects():
 			obj.queue_free()
 	spawned_objects.clear()	
 
+func clear_scene():
+	$AudioStreamPlayer.play()
+	$CanvasLayer/ClearText.modulate.a = 1.0
+	await get_tree().create_timer(3.0).timeout
+	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	
 	var debris = get_tree().get_nodes_in_group("debris").size()
 	$CanvasLayer/Label.text = "Debris: %d" % debris 
 
-	if debris == 0:
-		$CanvasLayer/ClearText.modulate.a = 1.0
+	if debris == 0 and clear_flag == false:
+		clear_flag = true
+		await get_tree().create_timer(1.0).timeout
+		clear_scene()
 
 	if Input.is_action_pressed("debug_1"):
 		#clear_objects()
